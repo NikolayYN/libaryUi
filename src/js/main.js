aff6066045c939094197ebd34c6d976a7c719c07
Nikolay Yuush <bawerek@gmail.com>
2020-07-02 15:41:44 +0300

    previous circle of the lib

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import Dom from './lib/core'
+
+//
+// const list = $('.list-order').html()
+// console.log(list);
+
+const afoo = new Dom();
+console.log(afoo);
+const fpp = afoo.$el('.list-order')
+console.log(fpp);
